47bfae918bb6d3cd97998b4c7d0b95e146f0a3c9
Rachèl Heimbach <rachel.heimbach@essent.nl>
2017-01-10 18:07:33 +0100

    Reformat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'eh-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'eh-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'eh works!';
+    title = 'eh works!';
 }
