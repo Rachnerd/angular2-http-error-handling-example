5e8814a9da0431f02c4a690ab98a66dee0615d0a
Rachèl Heimbach <rachel.heimbach@essent.nl>
2017-01-10 18:07:58 +0100

    users component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        UsersComponent
     ],
     imports: [
         BrowserModule,
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'eh-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
